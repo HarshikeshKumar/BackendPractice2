f6dcc71ba4109ad868584599175e8a4ccc7e634d
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-20 14:02:17 +0530

    Completed ExpressStarter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+
 const ServerConfig = require("./config/serverConfig.js");
 const connectDB = require("./config/dbConfig.js");
+
 const app = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
