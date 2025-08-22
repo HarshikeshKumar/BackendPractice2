da9905359306de563a6188eb7723e8098b00e8ec
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-23 01:36:46 +0530

    Basic inplementation og userControllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,12 @@
-function createUser() {
-  console.log("Controller called");
+function createUser(req, res) {
+  console.log("User Controller called");
+  console.log(req.body);
+
+  // TODO: Register the User Logic
+
+  return res.json({
+    message: "OK",
+  });
 }
 
 module.exports = {
