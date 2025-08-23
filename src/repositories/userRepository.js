64cb9d16811f3f2dcaa0e26abef074c55f0d8bec
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-23 23:46:41 +0530

    Started Debugging

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -2,13 +2,21 @@ const User = require("../schema/userSchema.js");
 
 class UserRepository {
   async findUser(parameters) {
-    const response = await User.findOne({ ...parameters });
-    return response;
+    try {
+      const response = await User.findOne({ ...parameters });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async createUser(userDetails) {
-    const response = await User.create(userDetails);
-    return response;
+    try {
+      const response = await User.create(userDetails);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
