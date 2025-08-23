17bb1ab2a08f4b5bd73153fe2c081c21fe08f00c
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-23 22:57:57 +0530

    Now Check in userService, If if user are already exists with same email and mobile number in DB

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,28 @@
-class userService {
-  registerUser(userDetails) {
+class UserService {
+  constructor(userRepository) {
+    // In the argument we will expect userRepository argument
+    this.userRepository = userRepository;
+  }
+
+  async registerUser(userDetails) {
     // It will create a brand new user in the db
+
     // 1. We need to check if the user with this email and mobile number already exists or not
+    const user = await this.userRepository.findUser({
+      email: userDetails.email,
+      mobileNumber: userDetails.mobileNumber,
+    });
+
+    if (user) {
+      // If we found a user
+      throw {
+        reason: "User with the given email and mobile number is already exists",
+        statusCode: 404,
+      };
+    }
     // 2. If not then create the user in the database
     // 3. return the detail of created user
   }
 }
+
+module.exports = UserService;
