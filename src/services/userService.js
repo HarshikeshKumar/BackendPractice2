af39e8eaa58ff481c03056d85258d63002bf4951
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-24 00:14:46 +0530

    Comment extra Logs which is use for debugging

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,7 +5,7 @@ class UserService {
   }
 
   async registerUser(userDetails) {
-    console.log("Hitting Service layer");
+    // console.log("Hitting Service layer");
     // It will create a brand new user in the db
 
     // 1. We need to check if the user with this email and mobile number already exists or not
