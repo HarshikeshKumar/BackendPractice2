c8d49ac3697be850e942db1d2c21bbf1bada4460
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-24 16:24:27 +0530

    Started The Implementation of AUTHENTICATION/LOGIN/SIGNIN with JWT Token

---
+++ src/controllers/authController.js
@@ -0,0 +1,9 @@
+async function login(req, res) {
+  const loginPayload = req.body;
+
+  // call authService
+}
+
+module.exports = {
+  login,
+};
---
+++ src/services/authService.js
@@ -0,0 +1,44 @@
+const { findUser } = require("../repositories/userRepository.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, JWT_EXPIRY } = require("../config/serverConfig.js");
+
+async function login(authDetails) {
+  const email = authDetails.email;
+  const plainPassword = authDetails.password;
+
+  // 1. Check if there is a registered user with the given email
+  const user = await findUser({ email });
+
+  if (!user) {
+    throw {
+      message: "No user found with the given email",
+      statusCode: 404,
+    };
+  }
+
+  // 2. If the user is found we need to compare plainIncommingPassword with hashedPassword
+  const isPasswordValidated = await bcrypt.compare(
+    plainPassword,
+    user.password
+  );
+
+  if (!isPasswordValidated) {
+    throw {
+      message: "Invalod password, Please try again",
+      statusCode: 401,
+    };
+  }
+
+  // 3. If the password is validated, create a token and return it
+
+  // 1st args--> Payload, 2nd args--> secretkey, 3rd args--> expiryTime
+  const token = jwt.sign({ email: user.email, id: user._id }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRY,
+  });
+  return token;
+}
+
+module.exports = {
+  login,
+};
