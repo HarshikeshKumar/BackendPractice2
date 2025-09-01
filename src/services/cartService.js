8a715567358872c8fe6555adccacbc384b59e37e
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-09-02 02:00:58 +0530

    Implemented addProductToCart functionality

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,5 +1,8 @@
 const { getCartByUserId } = require("../repositories/cartRepository.js");
 const NotFoundError = require("../utils/notFoundError.js");
+const { getProductById } = require("../repositories/productRepository.js");
+const BadRequestError = require("../utils/badRequestError.js");
+const AppError = require("../utils/appError.js");
 
 async function getCart(userId) {
   const cart = await getCartByUserId(userId);
@@ -9,6 +12,46 @@ async function getCart(userId) {
   return cart;
 }
 
+// Add to cart
+async function addToCart(userId, productId) {
+  const cart = await getCart(userId);
+  const product = await getProductById(productId);
+  if (!product) {
+    throw new NotFoundError("Product");
+  }
+  if (!product.inStock && product.quantity <= 0) {
+    throw new BadRequestError(["Product not available in stock"]);
+  }
+
+  // May be the product is already in stock
+  let foundProduct = false;
+  cart.items.forEach((item) => {
+    if (item.product._id == productId) {
+      if (product.quantity >= item.quantity + 1) {
+        item.quantity += 1;
+      } else {
+        throw new AppError(
+          "The quantity of the item requested is not available",
+          404
+        );
+      }
+
+      foundProduct = true;
+    }
+  });
+  if (!foundProduct) {
+    cart.items.push({
+      product: productId,
+      quantity: 1,
+    });
+  }
+
+  await cart.save();
+
+  return cart;
+}
+
 module.exports = {
   getCart,
+  addToCart,
 };
